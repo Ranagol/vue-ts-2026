9682eec7052c4c5789c48e6296d66e711fb0d3d6
Andor Horvat <andorhorvat@gmail.com>
2026-04-12 09:24:35 +0200

    Install axios, pinia, element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import { createPinia } from 'pinia'
 
 /**
  * 6 step: import the router from /router/index.ts
@@ -8,5 +11,7 @@ import App from './App.vue'
 import router from '@/router/index';
 
 createApp(App)
+    .use(createPinia())
+    .use(ElementPlus)
     .use(router)
     .mount('#app')
